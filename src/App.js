b73bb7b537dacd2cbaf0512bcc617c6345486683
Saheed Steven Adekunle <assmokson@gmail.com>
2023-07-05 19:33:00 -0500

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import "./App.css";
-import CampsitesList from "./features/campsites/CampsitesList.js";
-import { CAMPSITES } from "./app/shared/CAMPSITES";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,13 +1,12 @@
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
-import { Row, Col } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
-import selectAllCampsites from "./campsitesSlice";
+import { selectAllCampsites } from "./campsitesSlice";
 
 const CampsitesList = () => {
-  const campsites = selectAllCampsites;
+  const campsites = selectAllCampsites();
   return (
     <Row className="ms-auto">
-      {CAMPSITES.map((campsite) => {
+      {campsites.map((campsite) => {
         return (
           <Col md="5" className="m-4" key={campsite.id}>
             <CampsiteCard campsite={campsite} />
