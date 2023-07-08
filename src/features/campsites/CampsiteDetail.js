575764bdd99c4741f59b0dd45d0fcf5b5505b926
Saheed Steven Adekunle <assmokson@gmail.com>
2023-07-08 01:37:52 -0500

    Breadcrumbs SubHeader

--- src/features/campsites/CampsiteDetail.js
+++ src/features/campsites/CampsiteDetail.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody, Col } from "reactstrap";
 const CampsiteDetail = ({ campsite }) => {
   const { image, name, description } = campsite;
   return (
-    <Col className="m-4" md="12">
+    <Col className="m-1" md="5">
       <Card>
         <CardImg top src={image} alt={name} />
         <CardBody>
