ccfc7fc4c3aee56f990f1ded62ce9dd3a25f546a
Saheed Steven Adekunle <assmokson@gmail.com>
2023-07-28 18:34:56 -0500

    Error and Loading

--- src/features/campsites/CampsiteDetail.js
+++ src/features/campsites/CampsiteDetail.js
@@ -2,6 +2,7 @@ import { Card, CardImg, CardText, CardBody, Col } from "reactstrap";
 
 const CampsiteDetail = ({ campsite }) => {
   const { image, name, description } = campsite;
+
   return (
     <Col className="m-1" md="5">
       <Card>
